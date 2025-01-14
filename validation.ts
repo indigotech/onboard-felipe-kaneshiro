f21d58c7cf017ee15864c535f5a7f6e8cbac5246
Taqtile <taqtile@Corona.local>
2025-01-14 17:09:09 -0300

    change validation file

--- validation.ts
+++ validation.ts
@@ -1,18 +1,16 @@
 export const validateUserInput = (userData: { name: string; email: string; password: string; birthDate: string }) => {
   const { password } = userData;
+  const MAX_PASSWORD_LENGTH = 6;
 
-  //Password conditions validation
   const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d\s]+$/;
   if (!passwordRegex.test(password)) {
     throw new Error('Invalid password. The password must have at least 1 digit and 1 letter.');
   }
 
-  //Password length validation
-  if (password.length < 6) {
+  if (password.length < MAX_PASSWORD_LENGTH) {
     throw new Error('Invalid password. The password must have at least 6 characters');
   }
 
-  //No spaces key on password validation
   if (/\s/.test(password)) {
     throw new Error('Password must not contain spaces.');
   }
