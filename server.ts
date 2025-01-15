cc363ca29cd16007233133f53304d8ab746f83f1
Taqtile <taqtile@Corona.local>
2025-01-15 12:22:24 -0300

    remove files

--- server.ts
+++
@@ -1,8 +0,0 @@
-import { ApolloServer } from 'apollo-server';
-import { resolvers } from './resolvers/user-resolver';
-import { typeDefs } from './types/types';
-
-export const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
